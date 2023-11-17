b05bd67a0fffbe821516a0b9d337f9cce8621ba9
David Schinazi <dschinazi@google.com>
2023-11-17 15:34:08 -0800

    Autoformat code

--- src/iesg.py
+++ src/iesg.py
@@ -1,174 +1,197 @@
 #!/usr/bin/env python3
 
 import calendar
-from datetime import date
 import json
 import os
+from datetime import date
+
 from bs4 import BeautifulSoup
 
-metaDataDir = os.path.normpath(os.path.join(os.path.dirname(__file__), '..', 'data/meta'))
+metaDataDir = os.path.normpath(
+    os.path.join(os.path.dirname(__file__), "..", "data/meta")
+)
 if not os.path.exists(metaDataDir):
-  os.makedirs(metaDataDir)
+    os.makedirs(metaDataDir)
 
 # Using offline download pulled from this link:
 # https://www.ietf.org/how/meetings/past/
-pastIetfsFile = os.path.join(metaDataDir, 'IETF_Past_Meetings.html')
+pastIetfsFile = os.path.join(metaDataDir, "IETF_Past_Meetings.html")
 with open(pastIetfsFile) as fp:
-  pastIetfsSoup = BeautifulSoup(fp, "lxml")
+    pastIetfsSoup = BeautifulSoup(fp, "lxml")
 
 # Using offline download pulled from this link:
 # https://www.ietf.org/about/groups/iesg/past-members/
-pastIesgFile = os.path.join(metaDataDir, 'IESG_Past_Members.html')
+pastIesgFile = os.path.join(metaDataDir, "IESG_Past_Members.html")
 with open(pastIesgFile) as fp:
-  pastIesgSoup = BeautifulSoup(fp, "lxml")
+    pastIesgSoup = BeautifulSoup(fp, "lxml")
 
 ietfStartDates = {}
 ietfEndDates = {}
 
-for bp in pastIetfsSoup.body.main.find_all('div', class_='block-paragraph'):
-  if len(bp.text) < 1000:
-    continue
-  pastIetfElements = bp.contents
-  break
+for bp in pastIetfsSoup.body.main.find_all("div", class_="block-paragraph"):
+    if len(bp.text) < 1000:
+        continue
+    pastIetfElements = bp.contents
+    break
 
 months = {name: num for num, name in enumerate(calendar.month_name) if num}
 
 i = 0
 numPastIetfElements = len(pastIetfElements)
 while i + 1 < numPastIetfElements:
-  titleElement = pastIetfElements[i]
-  if titleElement.name == 'p':
-    i += 1
-    continue
-  assert titleElement.name == 'h2'
-  paragraphElement = pastIetfElements[i + 1]
-  assert paragraphElement.name == 'p'
-  titleText = titleElement.text
-  if titleText.startswith('IETF '):
-    ietfNum = int(titleText[5:])
-  else:
-    assert titleText.endswith(' IETF')
-    ietfNum = int(titleText[:-7])
-  assert ietfNum > 0
-  dateLine = paragraphElement.strings.__next__()
-  if dateLine[0].isdigit():
-    dls = dateLine.split(' ')
-    year = dls[2][:4]
-    startMonth = dls[1]
-    endMonth = startMonth
-    ds = dls[0].split('-')
-    startDay = ds[0]
-    endDay = ds[1]
-  else:
-    dls = dateLine.split(', ')
-    year = dls[1][:4]
-    ds = dls[0].split('-')
-    ss = ds[0].strip().split(' ')
-    startMonth = ss[0]
-    startDay = ss[1]
-    end = ds[1].strip()
-    if end.isdigit():
-      endMonth = startMonth
-      endDay = end
+    titleElement = pastIetfElements[i]
+    if titleElement.name == "p":
+        i += 1
+        continue
+    assert titleElement.name == "h2"
+    paragraphElement = pastIetfElements[i + 1]
+    assert paragraphElement.name == "p"
+    titleText = titleElement.text
+    if titleText.startswith("IETF "):
+        ietfNum = int(titleText[5:])
     else:
-      es = end.split(' ')
-      endMonth = es[0]
-      endDay = es[1]
-  ietfStartDates[ietfNum] = date(int(year), months[startMonth], int(startDay))
-  ietfEndDates[ietfNum] = date(int(year), months[endMonth], int(endDay))
-  # print('IETF {n}: {s} - {e}'.format(n=ietfNum, s=ietfStartDates[ietfNum], e=ietfEndDates[ietfNum]))
-  i += 2
+        assert titleText.endswith(" IETF")
+        ietfNum = int(titleText[:-7])
+    assert ietfNum > 0
+    dateLine = paragraphElement.strings.__next__()
+    if dateLine[0].isdigit():
+        dls = dateLine.split(" ")
+        year = dls[2][:4]
+        startMonth = dls[1]
+        endMonth = startMonth
+        ds = dls[0].split("-")
+        startDay = ds[0]
+        endDay = ds[1]
+    else:
+        dls = dateLine.split(", ")
+        year = dls[1][:4]
+        ds = dls[0].split("-")
+        ss = ds[0].strip().split(" ")
+        startMonth = ss[0]
+        startDay = ss[1]
+        end = ds[1].strip()
+        if end.isdigit():
+            endMonth = startMonth
+            endDay = end
+        else:
+            es = end.split(" ")
+            endMonth = es[0]
+            endDay = es[1]
+    ietfStartDates[ietfNum] = date(int(year), months[startMonth], int(startDay))
+    ietfEndDates[ietfNum] = date(int(year), months[endMonth], int(endDay))
+    # print('IETF {n}: {s} - {e}'.format(n=ietfNum, s=ietfStartDates[ietfNum], e=ietfEndDates[ietfNum]))
+    i += 2
 
 
 def is_block(css_class):
-  return css_class == 'block-heading' or css_class == 'block-paragraph'
+    return css_class == "block-heading" or css_class == "block-paragraph"
+
 
 def get_ietf_date(ietf, dates, minusOne=False):
-  s = ietf.split(' (')[0]
-  assert s.startswith('IETF ')
-  ietfNum = int(s[5:])
-  if minusOne:
-    ietfNum -= 1
-  return dates[ietfNum]
+    s = ietf.split(" (")[0]
+    assert s.startswith("IETF ")
+    ietfNum = int(s[5:])
+    if minusOne:
+        ietfNum -= 1
+    return dates[ietfNum]
 
-iesgBlocks = pastIesgSoup.body.main.find_all('div', class_=is_block)
+
+iesgBlocks = pastIesgSoup.body.main.find_all("div", class_=is_block)
 
 i = 0
 numIesgBlocks = len(iesgBlocks)
 while i < numIesgBlocks:
-  i += 1
-  if iesgBlocks[i - 1].text == 'Past IESG Members':
-    break
+    i += 1
+    if iesgBlocks[i - 1].text == "Past IESG Members":
+        break
 
 adIetfs = {}
 iesgs = []
 
 CURRENT_IETF = 117
-CURRENT_IESG = ['Andrew Alston', 'Roman Danyliw', 'Martin Duke', 'Lars Eggert',
-                'Erik Kline', 'Murray Kucherawy', 'Warren Kumari', 'Francesca Palombini',
-                'Zaheduzzaman Sarker', 'John Scudder', 'Éric Vyncke', 'Robert Wilton',
-                'Paul Wouters', 'Jim Guichard']
+CURRENT_IESG = [
+    "Andrew Alston",
+    "Roman Danyliw",
+    "Martin Duke",
+    "Lars Eggert",
+    "Erik Kline",
+    "Murray Kucherawy",
+    "Warren Kumari",
+    "Francesca Palombini",
+    "Zaheduzzaman Sarker",
+    "John Scudder",
+    "Éric Vyncke",
+    "Robert Wilton",
+    "Paul Wouters",
+    "Jim Guichard",
+]
 
-iesgs.append({
-    'date_start': ietfStartDates[CURRENT_IETF - 1],
-    'date_end': None,
-    'members': CURRENT_IESG,
-})
+iesgs.append(
+    {
+        "date_start": ietfStartDates[CURRENT_IETF - 1],
+        "date_end": None,
+        "members": CURRENT_IESG,
+    }
+)
 
 while i + 1 < numIesgBlocks:
-  timeframeBlock = iesgBlocks[i]
-  assert 'block-heading' in timeframeBlock['class']
-  membersBlock = iesgBlocks[i + 1]
-  assert 'block-paragraph' in membersBlock['class']
-  ietfs = set()
-  for ietfStr in timeframeBlock.text.split(', '):
-    ietfStr = ietfStr[:ietfStr.find(' (')]
-    assert ietfStr.startswith('IETF ')
-    ietfs.add(int(ietfStr[5:]))
-  print(timeframeBlock.text)
-  startDate = ietfStartDates[min(ietfs) - 1]
-  endDate = ietfEndDates[max(ietfs)]
-  print('timeframe {s} to {e}'.format(s=startDate, e=endDate))
-  names = []
-  for memberItem in membersBlock.find_all('li'):
-    memberDetails = memberItem.text.split(', ')
-    assert len(memberDetails) == 2
-    name = memberDetails[0]
-    name = name.replace(u"\u00A0", '')
-    names.append(name)
-    adSet = adIetfs.get(name, set())
-    adSet |= ietfs
-    adIetfs[name] = adSet
-    area = memberDetails[1]
-    print('  {a}: "{n}"'.format(a=area, n=name))
-  print(names)
-  iesgs.append({
-    'date_start': startDate,
-    'date_end': endDate,
-    'members': names,
-  })
-  i += 2
+    timeframeBlock = iesgBlocks[i]
+    assert "block-heading" in timeframeBlock["class"]
+    membersBlock = iesgBlocks[i + 1]
+    assert "block-paragraph" in membersBlock["class"]
+    ietfs = set()
+    for ietfStr in timeframeBlock.text.split(", "):
+        ietfStr = ietfStr[: ietfStr.find(" (")]
+        assert ietfStr.startswith("IETF ")
+        ietfs.add(int(ietfStr[5:]))
+    print(timeframeBlock.text)
+    startDate = ietfStartDates[min(ietfs) - 1]
+    endDate = ietfEndDates[max(ietfs)]
+    print("timeframe {s} to {e}".format(s=startDate, e=endDate))
+    names = []
+    for memberItem in membersBlock.find_all("li"):
+        memberDetails = memberItem.text.split(", ")
+        assert len(memberDetails) == 2
+        name = memberDetails[0]
+        name = name.replace("\u00A0", "")
+        names.append(name)
+        adSet = adIetfs.get(name, set())
+        adSet |= ietfs
+        adIetfs[name] = adSet
+        area = memberDetails[1]
+        print('  {a}: "{n}"'.format(a=area, n=name))
+    print(names)
+    iesgs.append(
+        {
+            "date_start": startDate,
+            "date_end": endDate,
+            "members": names,
+        }
+    )
+    i += 2
 
 for name in CURRENT_IESG:
-  adSet = adIetfs.get(name, set())
-  adSet.add(CURRENT_IETF)
-  adIetfs[name] = adSet
+    adSet = adIetfs.get(name, set())
+    adSet.add(CURRENT_IETF)
+    adIetfs[name] = adSet
 
 adEnds = {}
 for ad in adIetfs:
-  ietfs = adIetfs[ad]
-  adEnds[ad] = [ietfEndDates[i] for i in ietfs if i + 1 not in ietfs and i != CURRENT_IETF]
+    ietfs = adIetfs[ad]
+    adEnds[ad] = [
+        ietfEndDates[i] for i in ietfs if i + 1 not in ietfs and i != CURRENT_IETF
+    ]
 
 # for ad in adEnds:
 #   print('{ad}: {s}'.format(ad=ad, s=adEnds[ad]))
 
 # print(json.dumps(adEnds, indent=2, sort_keys=True, default=str))
 
-adEndsFile = os.path.join(metaDataDir, 'AD_term_ends.json')
+adEndsFile = os.path.join(metaDataDir, "AD_term_ends.json")
 with open(adEndsFile, "w") as f:
-  json.dump(adEnds, f, indent=2, sort_keys=True, default=str)
+    json.dump(adEnds, f, indent=2, sort_keys=True, default=str)
 
 
-iesgsFile = os.path.join(metaDataDir, 'IESGs.json')
+iesgsFile = os.path.join(metaDataDir, "IESGs.json")
 with open(iesgsFile, "w") as f:
-  json.dump(iesgs, f, indent=2, sort_keys=True, default=str)
+    json.dump(iesgs, f, indent=2, sort_keys=True, default=str)
--- src/rfc_info.py
+++ src/rfc_info.py
@@ -1,193 +1,223 @@
 #!/usr/bin/env python3
 
-from datetime import datetime, timezone
 import json
 import os
+from datetime import datetime, timezone
+from sys import argv, exit
+
 import requests
 from bs4 import BeautifulSoup
-from sys import argv, exit
 
 rfcNum = 9293
 
 if len(argv) > 1:
-  rfcNum = int(argv[1])
+    rfcNum = int(argv[1])
 
-resp = requests.get('http://localhost:8000/doc/rfc{}/history/'.format(rfcNum))
+resp = requests.get("http://localhost:8000/doc/rfc{}/history/".format(rfcNum))
 if resp.status_code == 404:
-  print('RFC {r} does not exist'.format(r=rfcNum))
-  exit(0)
+    print("RFC {r} does not exist".format(r=rfcNum))
+    exit(0)
 
 assert resp.ok
 soup = BeautifulSoup(resp.text, "lxml")
 
-if len(soup.find_all(lambda tag: tag.name == 'a' and 'IESG evaluation record' in tag.text)) == 0:
-  print('RFC {r} was not evaluated by the IESG'.format(r=rfcNum))
-  exit(0)
+if (
+    len(
+        soup.find_all(
+            lambda tag: tag.name == "a" and "IESG evaluation record" in tag.text
+        )
+    )
+    == 0
+):
+    print("RFC {r} was not evaluated by the IESG".format(r=rfcNum))
+    exit(0)
 
-metaDataDir = os.path.normpath(os.path.join(os.path.dirname(__file__), '..', 'data/meta'))
+metaDataDir = os.path.normpath(
+    os.path.join(os.path.dirname(__file__), "..", "data/meta")
+)
 
-adEndsFile = os.path.join(metaDataDir, 'AD_term_ends.json')
-with open(adEndsFile, 'r') as f:
-  adEnds = json.load(f)
+adEndsFile = os.path.join(metaDataDir, "AD_term_ends.json")
+with open(adEndsFile, "r") as f:
+    adEnds = json.load(f)
 
-iesgsFile = os.path.join(metaDataDir, 'IESGs.json')
-with open(iesgsFile, 'r') as f:
-  iesgsJ = json.load(f)
+iesgsFile = os.path.join(metaDataDir, "IESGs.json")
+with open(iesgsFile, "r") as f:
+    iesgsJ = json.load(f)
+
 
 def date_from_json(s):
-  if s is None:
-    return None
-  return datetime.strptime(s, '%Y-%m-%d').replace(tzinfo=timezone.utc)
+    if s is None:
+        return None
+    return datetime.strptime(s, "%Y-%m-%d").replace(tzinfo=timezone.utc)
+
 
 iesgs = []
 for iesg in iesgsJ:
-  iesgs.append({
-    'date_start': date_from_json(iesg['date_start']),
-    'date_end': date_from_json(iesg['date_end']),
-    'members': iesg['members'],
-  })
+    iesgs.append(
+        {
+            "date_start": date_from_json(iesg["date_start"]),
+            "date_end": date_from_json(iesg["date_end"]),
+            "members": iesg["members"],
+        }
+    )
 
 for ad in adEnds:
-  adEnds[ad] = [date_from_json(e) for e in adEnds[ad]]
-  # print('{ad}: {s}'.format(ad=ad, s=adEnds[ad]))
+    adEnds[ad] = [date_from_json(e) for e in adEnds[ad]]
+    # print('{ad}: {s}'.format(ad=ad, s=adEnds[ad]))
 
-#print(soup.prettify())
+# print(soup.prettify())
+
 
 class Ballot:
-  ad = None
-  ballotType = None
-  startTime = None
-  endTime = None
-  text = None
-  endReason = None
+    ad = None
+    ballotType = None
+    startTime = None
+    endTime = None
+    text = None
+    endReason = None
 
-  def __init__(self, ad, ballotType, startTime):
-    self.ad = ad
-    self.ballotType = ballotType
-    self.startTime = startTime
-  
-  def __str__(self):
-    return '{t} {s} {e} {txt}'.format(t=self.ballotType, s=self.startTime, e=self.endTime, txt=self.text if False else '')
+    def __init__(self, ad, ballotType, startTime):
+        self.ad = ad
+        self.ballotType = ballotType
+        self.startTime = startTime
+
+    def __str__(self):
+        return "{t} {s} {e} {txt}".format(
+            t=self.ballotType,
+            s=self.startTime,
+            e=self.endTime,
+            txt=self.text if False else "",
+        )
+
 
 ballots = {}
 ballotStartTime = None
 ballotEndTime = None
 
-rows = soup.body.table.tbody.findAll('tr')
+rows = soup.body.table.tbody.findAll("tr")
 for row in reversed(rows):
-  rowId = row['id']
-  cells = row.findAll('td')
-  timeStamp = datetime.strptime(cells[0].div['title'], '%Y-%m-%d %H:%M:%S %z')
-  revision = cells[1].text
-  author = cells[2].text
-  fullActionDivs = cells[3].find_all(attrs={"class": "full"})
-  if len(fullActionDivs) > 0:
-    action = ' '.join(fullActionDivs[0].strings)
-  else:
-    action = cells[3].text
-  if action == 'Created "Approve" ballot':
-    ballotStartTime = timeStamp
-    for iesg in iesgs:
-      if iesg['date_start'] > timeStamp:
-        continue
-      if iesg['date_end'] is not None and iesg['date_end'] < timeStamp:
-        continue
-      for ad in iesg['members']:
-        ballotsForAd = ballots.get(ad, [])
-        ballotsForAd.append(Ballot(ad, 'No Record', timeStamp))
-        ballots[ad] = ballotsForAd
-    # print('{t} - {a}'.format(t=timeStamp, a=action))
-  elif action == 'Closed "Approve" ballot':
-    ballotEndTime = timeStamp
-    for a in ballots:
-      ballots[a][-1].endTime = timeStamp
-      ballots[a][-1].endReason = 'evaluation_closed'
-  elif action.startswith('['):
-    actionType = action[1:action.find(']')]
-    if actionType == 'Ballot Position Update':
-      actionDetails = action[action.find(']') + 2:]
-      # print('{t} - {d}'.format(t=timeStamp, d=actionDetails))
-      if actionDetails.startswith('New position, '):
-        ballotType = actionDetails.split(', ')[1]
-        ballotsForAuthor = ballots.get(author, [])
-        if len(ballotsForAuthor) > 0:
-          ballotsForAuthor[-1].endTime = timeStamp
-          ballotsForAuthor[-1].endReason = 'new_position'
-        ballotsForAuthor.append(Ballot(author, ballotType, timeStamp))
-        ballots[author] = ballotsForAuthor
-      else:
-        changeSplit = actionDetails.split(' has been changed to ')
-        if len(changeSplit) > 1:
-          ballotType = changeSplit[1].split(' from ')[0]
-          ballotsForAuthor = ballots.get(author, [])
-          if len(ballotsForAuthor) > 0:
-            ballotsForAuthor[-1].endTime = timeStamp
-            ballotsForAuthor[-1].endReason = 'position_updated'
-          ballotsForAuthor.append(Ballot(author, ballotType, timeStamp))
-          ballots[author] = ballotsForAuthor
-    elif actionType == 'Ballot discuss':
-      actionDetails = action[action.find(']') + 2:]
-      ballotsForAuthor = ballots.get(author, [])
-      if len(ballotsForAuthor) > 0:
-        if ballots[author][-1].text is None:
-          ballots[author][-1].text = actionDetails
-        else:
-          ballotsForAuthor[-1].endTime = timeStamp
-          ballotsForAuthor[-1].endReason = 'discuss_updated'
-          newBallot = Ballot(author, ballots[author][-1].ballotType, timeStamp)
-          newBallot.text = actionDetails
-          ballotsForAuthor.append(newBallot)
-      ballots[author] = ballotsForAuthor
-    elif actionType == 'Ballot comment':
-      actionDetails = action[action.find(']') + 2:]
-      ballotsForAuthor = ballots.get(author, [])
-      if len(ballotsForAuthor) > 0 and ballots[author][-1].ballotType == 'Abstain':
-        if ballots[author][-1].text is None:
-          ballots[author][-1].text = actionDetails
-        else:
-          ballotsForAuthor[-1].endTime = timeStamp
-          ballotsForAuthor[-1].endReason = 'comment_updated'
-          newBallot = Ballot(author, ballots[author][-1].ballotType, timeStamp)
-          newBallot.text = actionDetails
-          ballotsForAuthor.append(newBallot)
-      ballots[author] = ballotsForAuthor
+    rowId = row["id"]
+    cells = row.findAll("td")
+    timeStamp = datetime.strptime(cells[0].div["title"], "%Y-%m-%d %H:%M:%S %z")
+    revision = cells[1].text
+    author = cells[2].text
+    fullActionDivs = cells[3].find_all(attrs={"class": "full"})
+    if len(fullActionDivs) > 0:
+        action = " ".join(fullActionDivs[0].strings)
+    else:
+        action = cells[3].text
+    if action == 'Created "Approve" ballot':
+        ballotStartTime = timeStamp
+        for iesg in iesgs:
+            if iesg["date_start"] > timeStamp:
+                continue
+            if iesg["date_end"] is not None and iesg["date_end"] < timeStamp:
+                continue
+            for ad in iesg["members"]:
+                ballotsForAd = ballots.get(ad, [])
+                ballotsForAd.append(Ballot(ad, "No Record", timeStamp))
+                ballots[ad] = ballotsForAd
+        # print('{t} - {a}'.format(t=timeStamp, a=action))
+    elif action == 'Closed "Approve" ballot':
+        ballotEndTime = timeStamp
+        for a in ballots:
+            ballots[a][-1].endTime = timeStamp
+            ballots[a][-1].endReason = "evaluation_closed"
+    elif action.startswith("["):
+        actionType = action[1 : action.find("]")]
+        if actionType == "Ballot Position Update":
+            actionDetails = action[action.find("]") + 2 :]
+            # print('{t} - {d}'.format(t=timeStamp, d=actionDetails))
+            if actionDetails.startswith("New position, "):
+                ballotType = actionDetails.split(", ")[1]
+                ballotsForAuthor = ballots.get(author, [])
+                if len(ballotsForAuthor) > 0:
+                    ballotsForAuthor[-1].endTime = timeStamp
+                    ballotsForAuthor[-1].endReason = "new_position"
+                ballotsForAuthor.append(Ballot(author, ballotType, timeStamp))
+                ballots[author] = ballotsForAuthor
+            else:
+                changeSplit = actionDetails.split(" has been changed to ")
+                if len(changeSplit) > 1:
+                    ballotType = changeSplit[1].split(" from ")[0]
+                    ballotsForAuthor = ballots.get(author, [])
+                    if len(ballotsForAuthor) > 0:
+                        ballotsForAuthor[-1].endTime = timeStamp
+                        ballotsForAuthor[-1].endReason = "position_updated"
+                    ballotsForAuthor.append(Ballot(author, ballotType, timeStamp))
+                    ballots[author] = ballotsForAuthor
+        elif actionType == "Ballot discuss":
+            actionDetails = action[action.find("]") + 2 :]
+            ballotsForAuthor = ballots.get(author, [])
+            if len(ballotsForAuthor) > 0:
+                if ballots[author][-1].text is None:
+                    ballots[author][-1].text = actionDetails
+                else:
+                    ballotsForAuthor[-1].endTime = timeStamp
+                    ballotsForAuthor[-1].endReason = "discuss_updated"
+                    newBallot = Ballot(
+                        author, ballots[author][-1].ballotType, timeStamp
+                    )
+                    newBallot.text = actionDetails
+                    ballotsForAuthor.append(newBallot)
+            ballots[author] = ballotsForAuthor
+        elif actionType == "Ballot comment":
+            actionDetails = action[action.find("]") + 2 :]
+            ballotsForAuthor = ballots.get(author, [])
+            if (
+                len(ballotsForAuthor) > 0
+                and ballots[author][-1].ballotType == "Abstain"
+            ):
+                if ballots[author][-1].text is None:
+                    ballots[author][-1].text = actionDetails
+                else:
+                    ballotsForAuthor[-1].endTime = timeStamp
+                    ballotsForAuthor[-1].endReason = "comment_updated"
+                    newBallot = Ballot(
+                        author, ballots[author][-1].ballotType, timeStamp
+                    )
+                    newBallot.text = actionDetails
+                    ballotsForAuthor.append(newBallot)
+            ballots[author] = ballotsForAuthor
 
 # print('ballotStartTime={}'.format(ballotStartTime))
 
 res = {}
-res['rfc'] = rfcNum
-res['evaluation_start'] = ballotStartTime
-res['evaluation_end'] = ballotEndTime
+res["rfc"] = rfcNum
+res["evaluation_start"] = ballotStartTime
+res["evaluation_end"] = ballotEndTime
 
 resBallots = {}
 
 for a in ballots:
-  # print(a)
-  resBallotsForAd = []
-  for b in ballots[a]:
-    for adEnd in adEnds[b.ad]:
-      if b.startTime < adEnd and (b.endTime is None or adEnd < b.endTime):
-        b.endTime = adEnd
-        b.endReason = "ad_term_ended"
-    resBallotsForAd.append({
-      'type': b.ballotType,
-      'start': b.startTime,
-      'end': b.endTime,
-      'end_reason': b.endReason,
-      'text': b.text,
-    })
-    # print('  {}'.format(b)) 
-  resBallots[a] = resBallotsForAd
+    # print(a)
+    resBallotsForAd = []
+    for b in ballots[a]:
+        for adEnd in adEnds[b.ad]:
+            if b.startTime < adEnd and (b.endTime is None or adEnd < b.endTime):
+                b.endTime = adEnd
+                b.endReason = "ad_term_ended"
+        resBallotsForAd.append(
+            {
+                "type": b.ballotType,
+                "start": b.startTime,
+                "end": b.endTime,
+                "end_reason": b.endReason,
+                "text": b.text,
+            }
+        )
+        # print('  {}'.format(b))
+    resBallots[a] = resBallotsForAd
 
-res['all_ballots'] = resBallots
+res["all_ballots"] = resBallots
 
-rfcDir = os.path.normpath(os.path.join(os.path.dirname(__file__), '..', 'data/rfc'))
+rfcDir = os.path.normpath(os.path.join(os.path.dirname(__file__), "..", "data/rfc"))
 if not os.path.exists(rfcDir):
-  os.makedirs(rfcDir)
+    os.makedirs(rfcDir)
 
-rfcFile = os.path.join(rfcDir, 'rfc{}.json'.format(rfcNum))
+rfcFile = os.path.join(rfcDir, "rfc{}.json".format(rfcNum))
 with open(rfcFile, "w") as f:
-  json.dump(res, f, indent=2, sort_keys=True, default=str)
+    json.dump(res, f, indent=2, sort_keys=True, default=str)
 
-print('RFC {r} analyzed succesfully'.format(r=rfcNum))
+print("RFC {r} analyzed succesfully".format(r=rfcNum))
 exit(0)
