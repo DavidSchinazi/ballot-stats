a95a079f621f4d7a8c15a25c919cdf01f22bdd30
David Schinazi <dschinazi@google.com>
2023-11-17 19:22:59 -0800

    Loop through RFCs

--- src/regenerate_rfc_ballots.py
+++ src/regenerate_rfc_ballots.py
@@ -5,7 +5,9 @@ from sys import argv
 from util.parse_ballots import parse_ballot
 
 if __name__ == "__main__":
-    rfc_num = 9293
     if len(argv) > 1:
         rfc_num = int(argv[1])
-    parse_ballot("rfc{r}".format(r=rfc_num))
+        parse_ballot("rfc{r}".format(r=rfc_num))
+    else:
+        for rfc_num in range(9600, 8500, -1):
+            parse_ballot("rfc{r}".format(r=rfc_num))
