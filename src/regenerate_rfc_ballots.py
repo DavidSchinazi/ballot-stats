3110eab54d146c1caa46cea4efd9139874e7c1b8
David Schinazi <dschinazi@google.com>
2023-11-17 19:19:37 -0800

    tweak

--- src/regenerate_rfc_ballots.py
+++ src/regenerate_rfc_ballots.py
@@ -4,9 +4,8 @@ from sys import argv
 
 from util.parse_ballots import parse_ballot
 
-rfc_num = 9293
-
-if len(argv) > 1:
-    rfc_num = int(argv[1])
-
-parse_ballot("rfc{r}".format(r=rfc_num))
+if __name__ == "__main__":
+    rfc_num = 9293
+    if len(argv) > 1:
+        rfc_num = int(argv[1])
+    parse_ballot("rfc{r}".format(r=rfc_num))
